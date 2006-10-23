409a4ee36b9102af2d6c1d324623227dcf831530
d0ccrazy <d0ccrazy@none>
2006-10-23 17:02:27 +0000

    Corrected level border algorithm (well, at least it looks good...)
    svn-rev: 48

--- src/level/generator.d
+++ src/level/generator.d
@@ -2,9 +2,10 @@ module level.generator;
 
 import level.level;
 import level.renderer;
-import level.genrandom;// : GenRandomLevel;
-import framework.framework;// : Surface;
-import utils.configfile;// : ConfigNode;
+import level.genrandom : GenRandomLevel;
+import framework.framework;
+import utils.configfile : ConfigNode;
+import utils.vector2;
 import filesystem;
 import std.stream;
 import str = std.string;
@@ -19,7 +20,7 @@ public class LevelGenerator {
     public void config(ConfigNode node) {
         mConfig = node;
     }
-    
+
     private static Lexel parseMarker(char[] value) {
         static char[][] marker_strings = ["FREE", "LAND", "SOLID_LAND"];
         static Lexel[] marker_values = [Lexel.FREE, Lexel.LAND,
@@ -32,7 +33,7 @@ public class LevelGenerator {
         //else explode
         throw new Exception("invalid marker value in configfile");
     }
-    
+
     private static Surface readTexture(char[] value, bool accept_null) {
         Surface res;
         if (value == "-") {
@@ -48,7 +49,7 @@ public class LevelGenerator {
         }
         return res;
     }
-    
+
     //some of this stuff maybe should be moved into configfile.d
     private template ReadListTemplate(T) {
          T[] readList(ConfigNode node, T delegate(char[] item) translate) {
@@ -61,7 +62,7 @@ public class LevelGenerator {
             return res;
          }
     }
-    
+
     private Vector2f[] readPointList(ConfigNode node) {
         //is that horrible or beautiful?
         return ReadListTemplate!(Vector2f).readList(node, (char[] item) {
@@ -82,7 +83,7 @@ public class LevelGenerator {
             return conv.toUint(item);
         });
     }
-    
+
     private bool parseColor(char[] s, Color color) {
         char[][] values = str.split(s);
         if (values.length < 3 || values.length > 4)
@@ -102,7 +103,7 @@ public class LevelGenerator {
         }
         return false;
     }
-    
+
     //maybe this should rather be implemented by genrandom.d?
     private void readParams(ConfigNode node, GenRandomLevel g) {
         //tedious, maybe should replaced by an associative array or so
@@ -138,44 +139,63 @@ public class LevelGenerator {
         if (tmp == tmp)
             g.config_remove_or_add = tmp;
     }
-    
+
     private Level generateRandom(uint width, uint height,
         ConfigNode template_node)
     {
+        Surface loadBorderTex(ConfigNode texNode)
+        {
+            Surface tex;
+            char[] tex_name = texNode.getStringValue("texture");
+            if (tex_name.length > 0) {
+                tex = readTexture(tex_name, false);
+            } else {
+                //sucky color-border hack
+                int height = texNode.getIntValue("height", 1);
+                tex = getFramework.createSurface(1, height);
+                auto col = Color(0,0,0);
+                parseColor(texNode.getStringValue("color"), col);
+                auto canvas = tex.startDraw();
+                canvas.drawFilledRect(Vector2i(0, 0), tex.size, col);
+                tex.endDraw();
+            }
+            return tex;
+        }
+
         auto gen = new GenRandomLevel(width, height);
-        
+
         if (template_node.getBoolValue("is_cave")) {
             auto tex = readTexture(template_node.getStringValue("texture"),
                 false);
             auto marker = parseMarker(template_node.getStringValue("marker"));
             gen.setAsCave(tex, marker);
         }
-        
+
         readParams(template_node, gen);
-        
+
         ConfigNode polys = template_node.getSubNode("polygons");
         foreach(char[] name, ConfigNode polygon; polys) {
             Vector2f[] points = readPointList(polygon.getSubNode("points"));
-            
+
             //scale the points
             for (uint i = 0; i < points.length; i++) {
                 points[i].x *= width;
                 points[i].y *= height;
             }
-            
+
             uint[] nosubdiv = readUIntList(polygon.getSubNode("nochange"));
             auto tex = readTexture(polygon.getStringValue("texture"), true);
             auto marker = parseMarker(polygon.getStringValue("marker"));
             auto visible = polygon.getBoolValue("visible", true);
             auto changeable = polygon.getBoolValue("changeable", true);
-            
+
             gen.addPolygon(points, nosubdiv, tex, marker, changeable, visible);
         }
-        
+
         auto ckey = Color(0,0,0,0);
         LevelRenderer renderer = new LevelRenderer(width, height, ckey);
         gen.generate(renderer);
-        
+
         //the least important part is the longest
         ConfigNode borders = template_node.getSubNode("borders");
         foreach(char[] name, ConfigNode border; borders) {
@@ -190,33 +210,26 @@ public class LevelGenerator {
             } else {
                 do_up = do_down = true;
             }
-            char[] tex_name = border.getStringValue("texture");
-            Surface tex;
-            if (tex_name.length > 0) {
-                tex = readTexture(tex_name, false);
+            Surface tex_up, tex_down;
+            if (border.findNode("texture_both")) {
+                tex_up = loadBorderTex(border.getSubNode("texture_both"));
+                tex_down = tex_up;
             } else {
-                //sucky color-border hack
-                int height = border.getIntValue("height", 1);
-                tex = getFramework.createSurface(1, height);
-                auto col = Color(0,0,0);
-                parseColor(border.getStringValue("color"), col);
-                auto canvas = tex.startDraw();
-                canvas.drawFilledRect(Vector2i(0, 0), tex.size, col);
-                tex.endDraw();
+                if (do_up)
+                    tex_up = loadBorderTex(border.getSubNode("texture_up"));
+                if (do_down)
+                    tex_down = loadBorderTex(border.getSubNode("texture_down"));
             }
-            if (do_up)
-                renderer.drawBorder(marker_a, marker_b, true, tex);
-            if (do_down)
-                renderer.drawBorder(marker_a, marker_b, false, tex);
+            renderer.drawBorder(marker_a, marker_b, do_up, do_down, tex_up, tex_down);
         }
-        
+
         debug {
             gen.dumpDebuggingStuff(renderer);
         }
-        
+
         return renderer.render();
     }
-    
+
     /// generate a random level based on a template
     public Level generateRandom(uint width, uint height, char[] templatename) {
         //search template
@@ -231,7 +244,7 @@ public class LevelGenerator {
             }
             count++;
         }
-        
+
         //not found, pick a random one instead
         uint pick = rand.rand() % count;
         foreach(char[] name, ConfigNode template_node; templates) {
@@ -240,7 +253,7 @@ public class LevelGenerator {
             }
             pick--;
         }
-        
+
         return null;
     }
 
