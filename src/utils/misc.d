f45747176efe0ee775899fecaa7de50db1f4ffb2
d0ccrazy <d0ccrazy@none>
2007-06-12 16:36:32 +0000

    alkshfalsdf
    svn-rev: 218

--- src/utils/misc.d
+++ src/utils/misc.d
@@ -187,3 +187,47 @@ class Factory(T, ConstructorArgs...) {
         return mConstructors.keys;
     }
 }
+
+static class StaticFactory(T, ConstructorArgs...) {
+    alias T delegate(ConstructorArgs) constructorCallback;
+    private static constructorCallback[char[]] mConstructors;
+
+    //can't be named register(), because... hm? DMD is just borken.
+    static void registerByDelegate(char[] name, constructorCallback create) {
+        if (name in mConstructors) {
+            throw new Exception("oh noes! class already exists: " ~ name);
+        }
+        mConstructors[name] = create;
+    }
+
+    //register by type
+    //call it like: Factory!(YourInterface).register!(YourClass)("name")
+    static void register(X)(char[] name) {
+        static assert( is (X : T));
+
+        //argh, using a function literal here crashes DMD *g*
+        T inst(ConstructorArgs x) {
+            return new X(x);
+        }
+
+        registerByDelegate(name, &inst);
+    }
+
+    //register using the unqualified class name
+    //named registerX() because overloading doesn't work
+    static void registerX(X)() {
+        register!(X)(X.stringof);
+    }
+
+    static T instantiate(char[] name, ConstructorArgs args) {
+        auto del = name in mConstructors;
+        if (!del) {
+            throw new Exception("class '"~name~"' not found.");
+        }
+        return (*del)(args);
+    }
+
+    static char[][] classes() {
+        return mConstructors.keys;
+    }
+}
