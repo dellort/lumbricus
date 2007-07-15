be1fbea77d8bd5a15a8706dbd5d94605b4b47bf3
d0ccrazy <d0ccrazy@none>
2007-07-15 18:48:19 +0000

    New blackhole flying animation
    svn-rev: 323

--- animutil/animutil.d
+++ animutil/animutil.d
@@ -19,7 +19,7 @@ ILuint loadImage(char[] filename) {
     return imgName;
 }
 
-ILuint rotateToNew(ILuint img, float angle) {
+ILuint rotateToNew(ILuint img, float angle, bool alpha) {
     ilBindImage(img);
     int w = ilGetInteger(IL_IMAGE_WIDTH);
     int h = ilGetInteger(IL_IMAGE_HEIGHT);
@@ -30,7 +30,10 @@ ILuint rotateToNew(ILuint img, float angle) {
 
     ilCopyImage(img);
 
-    //ilClearColour(0,0,0,0);
+    if (alpha)
+        ilClearColour(0,0,0,0);
+    else
+        ilClearColour(1,0,1,1);
 
     iluRotate(angle);
 
@@ -53,29 +56,41 @@ void saveImage(ILuint img, char[] filename) {
     }
 }
 
-ILuint newImage(int w, int h) {
+ILuint newImage(int w, int h, bool alpha) {
     ILuint imgName;
     ilGenImages(1, &imgName);
     ilBindImage(imgName);
 
-    ilTexImage(w, h, 1, 4, IL_RGBA, IL_UNSIGNED_BYTE, null);
+    if (alpha)
+        ilTexImage(w, h, 1, 4, IL_RGBA, IL_UNSIGNED_BYTE, null);
+    else
+        ilTexImage(w, h, 1, 3, IL_RGB, IL_UNSIGNED_BYTE, null);
 
     return imgName;
 }
 
-void blitAt(ILuint src, ILuint dst, int xsrc, int ysrc, int w, int h, int xdst, int ydst) {
+void blitAt(ILuint src, ILuint dst, int xsrc, int ysrc, int w, int h, int xdst,
+    int ydst, bool alpha)
+{
     ubyte[] buf;
     buf.length = w*h*4;
+    int type = IL_RGBA;
+    if (!alpha)
+        type = IL_RGB;
 
     ilBindImage(src);
-    ilCopyPixels(xsrc, ysrc, 0, w, h, 1, IL_RGBA, IL_UNSIGNED_BYTE, buf.ptr);
+    ilCopyPixels(xsrc, ysrc, 0, w, h, 1, type, IL_UNSIGNED_BYTE, buf.ptr);
 
     ilBindImage(dst);
-    ilSetPixels(xdst, ydst, 0, w, h, 1, IL_RGBA, IL_UNSIGNED_BYTE, buf.ptr);
+    ilSetPixels(xdst, ydst, 0, w, h, 1, type, IL_UNSIGNED_BYTE, buf.ptr);
 }
 
 int main(char[][] args)
 {
+    bool alpha = true;
+    if (args.length > 2)
+        alpha = false;
+
     DerelictIL.load();
     DerelictILU.load();
 
@@ -90,13 +105,13 @@ int main(char[][] args)
     ILuint[36] outImgs;
 
     for (int i = 0; i < 36; i++) {
-        outImgs[i] = rotateToNew(img, i*10);
+        outImgs[i] = rotateToNew(img, i*10, alpha);
         //saveImage("anim"~str.toString(i)~".png");
     }
 
-    ILuint finalImg = newImage(w*36,h);
+    ILuint finalImg = newImage(w*36, h, alpha);
     for (int i = 0; i < 36; i++) {
-        blitAt(outImgs[i], finalImg, 0, 0, w, h, w*i, 0);
+        blitAt(outImgs[i], finalImg, 0, 0, w, h, w*i, 0, alpha);
     }
     saveImage(finalImg, "anim.png");
 
