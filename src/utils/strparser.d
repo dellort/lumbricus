cadc4d7399db7069957630ce376e7c2509cd7adf
faulty <faulty@none>
2009-04-13 09:53:21 +0000

    waste time fighting the compiler and everything for trivial things - implemented two ways to get the names of enum items at runtime (both ways are   about as elegant as the pope roller skating in mecca):     . struct Enum type in misc.d, which is intended to replace builtin enums         good: have to type enum item only once         bad: it's horrible, and switch() doesn't work     . enumStrings() function in strparser.d         good: at least you can keep using normal D enums         bad: have to type enum item twice, need to ensure the function is called   which one is allowed to survive shall be decided by a duel to the death   (and yes, both are unused yet) - nothing else
    svn-rev: 687

--- src/utils/strparser.d
+++ src/utils/strparser.d
@@ -28,6 +28,15 @@ MyBox stringToBox(T)(char[] s) {
     return stringToBoxTypeID(typeid(T), s);
 }
 
+T stringToType(T)(char[] s) {
+    MyBox res = stringToBoxTypeID(typeid(T), s);
+    if (res.empty()) {
+        throw new Exception(myformat("can't parse '{}' to type {}", s,
+            typeid(T)));
+    }
+    return res.unbox!(T)();
+}
+
 char[] boxToString(MyBox box) {
     return gBoxUnParsers[box.type()](box);
 }
@@ -127,16 +136,104 @@ public char[] boxUnParseVector2i(MyBox b) {
     return myformat("{} {}", v.x, v.y);
 }
 
+/+
+Problem: get the names of enum items
+
+register all enum items at runtime:
+    void enumStrings(EnumType, char[] fields)();
+works like this:
+    enum E { x1, x2, x3 }
+    enumStrings!(E, "x1,x2,x3");
+the nice thing is that the strings are tested at compile time if they are valid
+enum items, and the enum value is correctly obtained even if the order is
+different etc.
+
+can't deal with whitespace in the fields-string correctly
++/
+
+private struct EnumItem {
+    char[] name;
+    int value;
+}
+
+private static EnumItem[] enum_get_vals(EnumType, char[] fields)() {
+    EnumType X;
+    static char[] gen_code() {
+        char[][] pfields = ctfe_split(fields, ',');
+        char[] code = "[";
+        foreach (int idx, s; pfields) {
+            if (idx != 0)
+                code ~= ",";
+            code ~= `EnumItem("` ~ s ~ `", typeof(X)` ~ "." ~ s ~ `)`;
+        }
+        code ~= "]";
+        return code;
+    }
+    mixin(`return ` ~ gen_code() ~ `;`);
+}
+
+void enumStrings(EnumType, char[] fields)() {
+    static assert(is(EnumType == enum));
+
+    static const EnumItem[] items = enum_get_vals!(EnumType, fields)();
+
+    static char[] box_unparse_enum(MyBox b) {
+        EnumType val = b.unbox!(EnumType)();
+        assert(val >= EnumType.min && val <= EnumType.max);
+        foreach (EnumItem e; items) {
+            if (e.value == val)
+                return e.name;
+        }
+        //internal error
+        assert(false, myformat("undefined enum value: ", val));
+    }
+
+    static MyBox box_parse_enum(char[] s) {
+        foreach (EnumItem e; items) {
+            if (e.name == s)
+                return MyBox.Box!(EnumType)(cast(EnumType)e.value);
+        }
+        return MyBox();
+    }
+
+    //don't do all the work again (might not work as intended, of course)
+    if (typeid(EnumType) in gBoxParsers)
+        return;
+
+    //sanity test: make sure all valid enum values are covered
+    //not sure if this the right thing to do, because you can't really enumerate
+    //all enum items, you only have .max and .min
+    bool[] covered;
+    covered.length = EnumType.max - EnumType.min + 1;
+    foreach (EnumItem e; items) {
+        covered[e.value - EnumType.min] = true;
+    }
+    foreach (int idx, bool b; covered) {
+        assert(b, myformat("for type {}, enum item with numerical value {} is"
+            " not covered by enumStrings() call, field args: '{}'",
+            typeid(EnumType), idx + EnumType.min, fields));
+    }
+
+    gBoxUnParsers[typeid(EnumType)] = &box_unparse_enum;
+    gBoxParsers[typeid(EnumType)] = &box_parse_enum;
+}
+
 unittest {
     assert(boxParseInt("123").unbox!(int) == 123);
     assert(boxParseInt("abc").type is null);
     assert(boxParseInt("1abc").type is null);
     assert(boxParseInt("").type is null);
+    //bug or feature?
+    assert(boxParseInt(" 123").unbox!(int) == 123);
+    assert(boxParseInt("123 ").type is null);
 
     assert(boxParseFloat("123.25").unbox!(float) == 123.25f);
     assert(boxParseFloat("abc").type is null);
     assert(boxParseFloat("1.0abc").type is null);
     assert(boxParseFloat("").type is null);
+    //same behaviour as with int
+    assert(boxParseFloat(" 123").unbox!(float) == 123f);
+    assert(boxParseFloat("123 ").type is null);
 
     assert(boxParseBool("false").unbox!(bool) == false);
     assert(boxParseBool("yes").unbox!(bool) == true);
@@ -150,6 +247,30 @@ unittest {
     debug testParse("1 2");
     debug testParse("1 2.0");
     +/
+
+    //---- for the enum crap
+
+    //(LOL DMD: directly defining the enum without struct X causes linker error)
+    struct X {
+        enum BlaTest {
+            x1=5,
+            fgf,
+            x2,
+        }
+    }
+    alias X.BlaTest BlaTest;
+
+    //should fail at runtime with an assertion (missing value)
+    //-- enumStrings!(BlaTest, "x1,x2");
+    //should fail at compile time (can't find doesnotexist)
+    //-- enumStrings!(BlaTest, "x1,doesnotexist");
+
+    enumStrings!(BlaTest, "x1,fgf,x2");
+    assert(stringToType!(BlaTest)("x1") == BlaTest.x1);
+    assert(stringToType!(BlaTest)("x2") == BlaTest.x2);
+    assert(boxToString(MyBox.Box(BlaTest.fgf)) == "fgf");
+
+    //----
 
     debug Trace.formatln("strparser.d unittest: passed.");
 }
