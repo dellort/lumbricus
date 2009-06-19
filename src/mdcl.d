75ef5989ab1425085ce7ff99eecdb0a3b3dc7c9a
faulty <faulty@none>
2009-06-19 00:37:33 +0000

    just linking in minid per svn externals mdcl.d is copied from minid svn, because a single file can't be external (?)
    svn-rev: 803

---
+++ src/mdcl.d
@@ -0,0 +1,45 @@
+//Source: http://dsource.org/projects/minid/browser/trunk/mdcl.d r520
+/******************************************************************************
+License:
+Copyright (c) 2008 Jarrett Billingsley
+
+This software is provided 'as-is', without any express or implied warranty.
+In no event will the authors be held liable for any damages arising from the
+use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it freely,
+subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+	claim that you wrote the original software. If you use this software in a
+	product, an acknowledgment in the product documentation would be
+	appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not
+	be misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source distribution.
+******************************************************************************/
+
+module mdcl;
+
+import tango.io.Stdout;
+import tango.io.Console;
+
+import minid.api;
+import minid.commandline;
+
+// import minid.addons.sdl;
+// import minid.addons.gl;
+
+void main(char[][] args)
+{
+	MDVM vm;
+	auto t = openVM(&vm);
+	loadStdlibs(t, MDStdlib.All);
+// 	SdlLib.init(t);
+// 	GlLib.init(t);
+
+	CommandLine().run(t, args);
+}
